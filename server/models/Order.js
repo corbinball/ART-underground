cbc3290d0a7fe34600707119786c1d0baa401b9a
Andrew Tran <tranandrew@gmail.com>
2022-01-24 19:42:54 -0600

    Debug website for local launch. Order.js and seeds.js

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,82 +1,94 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const orderSchema = new Schema({
-  shippingInfo: {
-    address: {
-      type: String,
-      required: [true, "Please enter an address"],
-    },
-    city: {
-      type: String,
-      required: [true, "Please enter a city"],
-    },
-    state: {
-      type: String,
-      required: [true, "Please enter a state"],
-    },
-    postalCode: {
-      type: Number,
-      required: [true, "Please enter a postal code"],
-      minLength: 5,
-    },
-    phone: {
-      type: Number,
-      required: [true, "Please enter a phone number"],
-    },
+  purchaseDate: {
+    type: Date,
+    default: Date.now,
   },
-
-  orderItems: [
+  products: [
     {
-      name: {
-        type: String,
-        required: true,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
-      productId: {
-        type: mongoose.Schema.ObjectId,
-        ref: "Product",
-        required: true,
-      },
+      type: Schema.Types.ObjectId,
+      ref: "Product",
     },
   ],
-
-  userId: {
-    type: mongoose.Schema.ObjectId,
-      ref: "User",
-      required: true,
-      },
-    itemsPrice: {
-      type: Number,
-      required: true,
-    },
-    shippingPrice: {
-      type: Number,
-      required: true,
-    },
-    totalPrice: {
-      type: Number,
-      required: true,
-    },
-    dateAdded: {
-      type: Date,
-      default: Date.now,
-    },
-    paymentMethod: {
-      status: {type: String},
-      required: [true, "Please enter a payment method"]
-    },
 });
-  
 
+const Order = mongoose.model("Order", orderSchema);
 
-module.exports  = mongoose.model('Order', orderSchema);
+module.exports = Order;
 
+// module.exports = mongoose.model("Order", orderSchema);
+
+//   shippingInfo: {
+//     address: {
+//       type: String,
+//       required: [true, "Please enter an address"],
+//     },
+//     city: {
+//       type: String,
+//       required: [true, "Please enter a city"],
+//     },
+//     state: {
+//       type: String,
+//       required: [true, "Please enter a state"],
+//     },
+//     postalCode: {
+//       type: Number,
+//       required: [true, "Please enter a postal code"],
+//       minLength: 5,
+//     },
+//     phone: {
+//       type: Number,
+//       required: [true, "Please enter a phone number"],
+//     },
+//   },
+
+// orderItems: [
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     price: {
+//       type: Number,
+//       required: true,
+//     },
+//     quantity: {
+//       type: Number,
+//       required: true,
+//     },
+//     productId: {
+//       type: mongoose.Schema.ObjectId,
+//       ref: "Product",
+//       required: true,
+//     },
+//   },
+// ],
+
+// userId: {
+//   type: mongoose.Schema.ObjectId,
+//   ref: "User",
+//   required: true,
+// },
+// itemsPrice: {
+//   type: Number,
+//   required: true,
+// },
+// shippingPrice: {
+//   type: Number,
+//   required: true,
+// },
+// totalPrice: {
+//   type: Number,
+//   required: true,
+// },
+// dateAdded: {
+//   type: Date,
+//   default: Date.now,
+// },
+// paymentMethod: {
+//   status: { type: String },
+//   required: [true, "Please enter a payment method"],
+// },

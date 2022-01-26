df7a881e03a3d72f3f1f0dfaebce11d7b476b2dd
Andrew Tran <tranandrew@gmail.com>
2022-01-26 17:06:00 -0600

    Added sliders. create 3 different sliders for each type of art

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
+import ImageCarousel from "../components/Carousel/ImageCarousel";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <CategoryMenu />
       <ProductList />
       <Cart />
+      <ImageCarousel />
     </div>
   );
 };
